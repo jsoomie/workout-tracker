741c7d7df4eff2e007e3ed69a63de92c375bc794
Jonathan Soukaseume <sksmejn@gmail.com>
2021-04-10 08:04:13 -0700

    added routes to the other pages

--- controllers/router.js
+++ controllers/router.js
@@ -1,7 +1,21 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", {
+        title: "Fitness Pal",
+    });
+});
+
+router.get("/exercise", (req, res) => {
+    res.render("exercise", {
+        title: "Exercises!",
+    });
+});
+
+router.get("/stats", (req, res) => {
+    res.render("stats", {
+        title: "Your stats!",
+    });
 });
 
 module.exports = router;
